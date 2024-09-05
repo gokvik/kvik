bc28cb137131e4d26f6411bf191f2ca308e7ab30
Dávid Benko <davidbenko@davidbenko.dev>
2024-09-05 08:54:54 +0200

    Node: copy, leave only `INode` interface
    Other interfaces don't make much sense, because revised nodes will have
    various combinations of (multiple) local layers and remote layer.

---
+++ include/kvik/node.hpp
@@ -0,0 +1,97 @@
+/**
+ * @file node.hpp
+ * @author Dávid Benko (davidbenko@davidbenko.dev)
+ * @brief Node interface for Kvik
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#pragma once
+
+#include <functional>
+
+#include "kvik/errors.hpp"
+
+namespace kvik
+{
+    /**
+     * @brief Generic configuration for any node type
+     */
+    struct GenericNodeConfig
+    {
+        struct Reporting
+        {
+            std::string baseTopic = "_report";                  //!< Base topic for reporting purposes
+            std::string rssiSubtopic = "rssi";                  //!< Subtopic for RSSI reporting
+            std::string probePayloadSubtopic = "probe_payload"; //!< Subtopic for probe payload reporting
+        };
+
+        struct TopicSeparators
+        {
+            std::string levelSeparator = "/";      //!< Separator used between topic parts
+            std::string singleLevelWildcard = "+"; //!< Token used as single level wildcard
+            std::string multiLevelWildcard = "#";  //!< Token used as multi level wildcard
+        };
+
+        Reporting reporting;
+        TopicSeparators topicSep;
+    };
+
+    /**
+     * @brief Subscribe callback type
+     */
+    using SubscribeCb = std::function<void(const std::string &topic,
+                                           const std::string &payload)>;
+
+    /**
+     * @brief Interface for generic node type of Kvik
+     */
+    class INode
+    {
+    public:
+        /**
+         * @brief Constructs a new generic node
+         */
+        INode();
+
+        /**
+         * @brief Publishes payload to topic
+         *
+         * This is primary endpoint for publishing data locally on all node types.
+         *
+         * @param topic Topic
+         * @param payload Payload
+         * @return Error code
+         */
+        virtual ErrCode publish(const std::string &topic,
+                                const std::string &payload) = 0;
+
+        /**
+         * @brief Subscribes to topic
+         *
+         * This is primary endpoint for subscribing locally on all node types.
+         *
+         * @param topic Topic
+         * @param cb Callback function
+         * @return Error code
+         */
+        virtual ErrCode subscribe(const std::string &topic, SubscribeCb cb) = 0;
+
+        /**
+         * @brief Unsubscribes from topic
+         *
+         * This is primary endpoint for unsubscribing locally on all node types.
+         *
+         * @param topic Topic
+         * @return Error code
+         */
+        virtual ErrCode unsubscribe(const std::string &topic) = 0;
+
+        /**
+         * @brief Resubscribes to all topics
+         * @return Error code
+         */
+        virtual ErrCode resubscribeAll() = 0;
+    };
+} // namespace kvik
---
+++ src/common/node.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file node.cpp
+ * @author Dávid Benko (davidbenko@davidbenko.dev)
+ * @brief Node interface for Kvik
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#pragma once
+
+#include "kvik/logger.hpp"
+#include "kvik/node.hpp"
+#include "kvik/version.hpp"
+
+// Log tag
+static const char* KVIK_LOG_TAG = "Kvik/Node";
+
+namespace kvik
+{
+    INode::INode()
+    {
+        KVIK_LOGI("Kvik version: %s", kvik::VERSION);
+    }
+} // namespace kvik
