f1cda2d344428db6962fc7f5c86fc677b5ef8b18
Dávid Benko <davidbenko@davidbenko.dev>
2024-09-17 12:17:01 +0200

    Testing/LocalMsg: add tests for stringification of msg type and fail reason

--- test/tests/local_msg.cpp
+++ test/tests/local_msg.cpp
@@ -76,3 +76,38 @@ TEST_CASE("Comparison", "[LocalMsg]")
         REQUIRE(msg1 == msg2);
     }
 }
+
+TEST_CASE("String representation", "[LocalMsgType]")
+{
+    auto strEq = [](LocalMsgType type,
+                    std::string str) -> bool {
+        return std::string(localMsgTypeToStr(type)) == str;
+    };
+
+    CHECK(strEq(LocalMsgType::NONE, "NONE"));
+    CHECK(strEq(LocalMsgType::OK, "OK"));
+    CHECK(strEq(LocalMsgType::FAIL, "FAIL"));
+    CHECK(strEq(LocalMsgType::PROBE_REQ, "PROBE_REQ"));
+    CHECK(strEq(LocalMsgType::PROBE_RES, "PROBE_RES"));
+    CHECK(strEq(LocalMsgType::PUB_SUB_UNSUB, "PUB_SUB_UNSUB"));
+    CHECK(strEq(LocalMsgType::SUB_DATA, "SUB_DATA"));
+
+    // Invalid value
+    CHECK(strEq(static_cast<LocalMsgType>(-1), "???"));
+}
+
+TEST_CASE("String representation", "[LocalMsgFailReason]")
+{
+    auto strEq = [](LocalMsgFailReason reason, std::string str) -> bool {
+        return std::string(localMsgFailReasonToStr(reason)) == str;
+    };
+
+    CHECK(strEq(LocalMsgFailReason::NONE, "NONE"));
+    CHECK(strEq(LocalMsgFailReason::DUP_ID, "DUP_ID"));
+    CHECK(strEq(LocalMsgFailReason::INVALID_TS, "INVALID_TS"));
+    CHECK(strEq(LocalMsgFailReason::PROCESSING_FAILED, "PROCESSING_FAILED"));
+    CHECK(strEq(LocalMsgFailReason::UNKNOWN_SENDER, "UNKNOWN_SENDER"));
+
+    // Invalid value
+    CHECK(strEq(static_cast<LocalMsgFailReason>(-1), "???"));
+}
